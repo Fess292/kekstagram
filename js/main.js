093f3d86a2b5a15a15f40b8505d1419c0321a746
Paul Parshin <fess292@gmail.com>
2023-03-20 00:27:44 +0300

    check first homework

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-function randomNumber(min, max) {
-  if ((min >= 0) && (max >= 0)) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  } else {
-    console.log('введите корректное значение');
-  }
+// моя функция выбора рандомного числа
+// function randomNumber(min, max) {
+//   if ((min >= 0) && (max >= 0)) {
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rand);
+//   } else {
+//     console.log('введите корректное значение');
+//   }
+// }
+// randomNumber(1, 3);
+
+// код школы
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
-randomNumber(1, 3);
+getRandomPositiveInteger(-100,10);
 
 function checkLine(line, maxLong) {
   if (line.length <= maxLong ){
@@ -16,4 +26,10 @@ function checkLine(line, maxLong) {
   }
 }
 checkLine('привет', 2);
+
+// код школы
+// function checkStringLength (string, length) {
+//   return string.length <= length;
+// }
+// checkStringLength('привет', 2);
 
